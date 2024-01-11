962382256adba1e63c4c16d284ab3e0992957fb6
Rapilias <rapilias.rest@gmail.com>
2024-01-12 04:03:41 +0900

    Fix ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,4 @@
 # dotnet tool restore
 dotnet DocFxTocGenerator --docfolder ./docs --outputfolder ./docs && dotnet run --project docfx-toc-tools/docfx-toc-tools.csproj
 # dotnet docfx ./docs/docfx.json --serve
+dotnet docfx build ./docs/docfx.json
